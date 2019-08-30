077d32786d1e57126222954b28013afb7eacbe87
Cody Scott <cody.j.b.scott@gmail.com>
2019-08-30 11:01:59 -0400

    Remove graphene extensions

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from graphql_extensions.views import GraphQLView
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
